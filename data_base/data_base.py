fcfe317b512e0b6bb3b7f9d5150a98c3bf2c468a
João Gabriel Iuzviak Mantagute <joaogabriel@Joaos-MacBook-Air.local>
2025-07-19 09:09:47 -0300

    update (db)

--- data_base/data_base.py
+++ data_base/data_base.py
@@ -1,7 +1,19 @@
 from peewee import PostgresqlDatabase
 from dotenv import load_dotenv
-import os 
+from urllib.parse import urlparse, parse_qs
+import os
 
 load_dotenv()
 
-db = PostgresqlDatabase(os.getenv('DATA_BASE_URL', ''))
+url = urlparse(os.getenv("DATA_BASE_URL", ""))
+
+query_params = {k: v[0] for k, v in parse_qs(url.query).items()}
+
+db = PostgresqlDatabase(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port,
+    **query_params
+)
